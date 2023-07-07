028f8fa6f0bb8a472a19940d4bfa7049801ac0e4
HOANG Trung Thong <thoang5@mantu.com>
2023-07-07 14:39:16 +0700

    like record

--- src/partials/community/FeedPosts.jsx
+++ src/partials/community/FeedPosts.jsx
@@ -327,25 +327,21 @@ const FeedPosts = forwardRef((props, ref) => {
                 {/* Footer */}
                 <footer className="flex items-center space-x-4">
                   {/* Like button */}
-                  <button className="flex items-center text-indigo-400">
+                  <button
+                  onClick={() => likeNewsfeed(n.id)}
+                  className={"flex items-center " + `${n.record.isLiked ? "text-indigo-500" : "text-slate-400"}`}>
+                    {
+                    n.record.isLiked ?   
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-heart-filled text-indigo-500 mr-1.5" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                      <path d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z" stroke-width="0" fill="currentColor" />
+                    </svg>
+                    :
                     <svg className="w-4 h-4 shrink-0 fill-current mr-1.5" viewBox="0 0 16 16">
                       <path d="M14.682 2.318A4.485 4.485 0 0011.5 1 4.377 4.377 0 008 2.707 4.383 4.383 0 004.5 1a4.5 4.5 0 00-3.182 7.682L8 15l6.682-6.318a4.5 4.5 0 000-6.364zm-1.4 4.933L8 12.247l-5.285-5A2.5 2.5 0 014.5 3c1.437 0 2.312.681 3.5 2.625C9.187 3.681 10.062 3 11.5 3a2.5 2.5 0 011.785 4.251h-.003z" />
                     </svg>
-                    <div className="text-sm text-indigo-500">2.2K</div>
-                  </button>
-                  {/* Share button */}
-                  <button className="flex items-center text-slate-400 hover:text-indigo-500">
-                    <svg className="w-4 h-4 shrink-0 fill-current mr-1.5" viewBox="0 0 16 16">
-                      <path d="M13 7h2v6a1 1 0 0 1-1 1H4v2l-4-3 4-3v2h9V7ZM3 9H1V3a1 1 0 0 1 1-1h10V0l4 3-4 3V4H3v5Z" />
-                    </svg>
-                    <div className="text-sm text-slate-500">4.3K</div>
-                  </button>
-                  {/* Replies button */}
-                  <button className="flex items-center text-slate-400 hover:text-indigo-500">
-                    <svg className="w-4 h-4 shrink-0 fill-current mr-1.5" viewBox="0 0 16 16">
-                      <path d="M8 0C3.6 0 0 3.1 0 7s3.6 7 8 7h.6l5.4 2v-4.4c1.2-1.2 2-2.8 2-4.6 0-3.9-3.6-7-8-7zm4 10.8v2.3L8.9 12H8c-3.3 0-6-2.2-6-5s2.7-5 6-5 6 2.2 6 5c0 2.2-2 3.8-2 3.8z" />
-                    </svg>
-                    <div className="text-sm text-slate-500">92</div>
+                  }
+                    <div className={"text-sm "  + `${n.record.isLiked ? "text-indigo-500" : "text-slate-400"}`}>{ n.record.likeCount ?? 0 }</div>
                   </button>
                 </footer>
               </div>
