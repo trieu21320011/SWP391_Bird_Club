ee1bf036e61951a573e98bcb331f00f6bc4ba69b
son43334 <Sonnguyen43334@gmail.com>
2023-07-11 23:18:01 +0700

    son_last

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -66,7 +66,7 @@ function Dashboard() {
               <a class="px-4 py-2 text-sm text-white shadow-sm border-transparent bg-teal-600 hover:bg-teal-700 focus:ring-teal-500 inline-flex items-center border font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2" href="/signin">
                 Join Us
               </a>
-              <a class="px-4 py-2 text-sm border-gray-300 text-gray-700 bg-white hover:border-gray-500 focus:ring-teal-500 inline-flex items-center border font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2" href="/event">
+              <a class="px-4 py-2 text-sm border-gray-300 text-gray-700 bg-white hover:border-gray-500 focus:ring-teal-500 inline-flex items-center border font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2" href="/signin">
                 Tell me more
               </a>
             </div>
